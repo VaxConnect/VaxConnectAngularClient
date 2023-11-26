f2aadf2dd0bd3481144337160a68be4a86dd8103
Marco <marco.pertegal.prieto@gmail.com>
2023-11-26 13:09:53 +0100

    patient list fix

--- src/app/components/CALENDARPAGE/user/nav/nav.component.ts
+++ src/app/components/CALENDARPAGE/user/nav/nav.component.ts
@@ -9,18 +9,22 @@ import { patientBasicDataResponse } from '../../../../models/patient-data.interf
   styleUrl: './nav.component.css'
 })
 export class NavComponent implements OnInit {
-  dependentList: patientBasicDataResponse[] = [];
+  patientList: patientBasicDataResponse[] = [];
   userLogged!: patientBasicDataResponse;
   active = 1;
 
   constructor(private patientService: PatientService) { }
 
   ngOnInit(): void {
-    this.patientService.GetUserDependents().subscribe(resp => {
-      this.dependentList = resp;
-    });
+
     this.patientService.GetUserLogged().subscribe(resp => {
       this.userLogged = resp;
-    })
+      this.patientList.push(this.userLogged);
+
+      this.patientService.GetUserDependents().subscribe(resp => {
+        this.patientList = this.patientList.concat(resp);
+      });
+
+    });
   }
 }
